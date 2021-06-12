092a9d481e3815a7814231d387d94e5497c857bc
Luca Davanzo <davanzo.luca@gmail.com>
2021-06-12 13:08:20 +0200

    Use 16 cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Game from "./pages/game";
 function App() {
     return (
         <div className="App">
-            <Game cardNumber={4}/>
+            <Game cardNumber={16}/>
         </div>
     );
 }
